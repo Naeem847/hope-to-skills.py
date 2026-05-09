34fcca861572aa9242d829e548509f748e5e4ff6
Naeem847 <ashoornaeem@gmail.com>
2026-05-09 10:15:03 +0500

    practice python basic 1A Python practice project covering basic programming concepts, including string manipulation, conditional statements, and NumPy operations. The project demonstrates real-world beginner-level coding tasks such as reversing strings, counting vowels, checking even/odd numbers, and sorting a list using NumPy.. reverser text 2 using for loop and conditional statement like elif and also using numpy

--- string.py
+++ string.py
@@ -1,1 +1,34 @@
+# # # # 1. The string in reverse order.
+text=str(input("enter your text:"))
 
+reverse_text=text[::-1]
+print("reversed string is:",reverse_text)
+
+
+vowels="aeiouAEIOU"
+vowels_count  =0
+
+for char in text:
+    if char in vowels:
+        vowels_count  +=1
+
+print("reversed string:",reverse_text)
+print("number of vowels in the string:",vowels_count)
+
+# # #  2: Hands-on Coding Project
+
+num=int(input("enter a number:"))
+if num%2==0:
+    print(f"the number {num} is even:")
+else:
+    print(f"the number{num} is odd:")
+# # # # 3: Hands-on Coding Project
+import numpy as np
+
+numbers = list(map(int, input("Enter integers separated by spaces: ").split()))
+
+#6: Sort the list using numpy.sort()
+sorted_numbers = np.sort(numbers)
+
+#7: Print the sorted list
+print("Sorted list:", sorted_numbers)
