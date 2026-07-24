4bbc14078e1364f1f1812aa6d0eac3c64b37b3ac
Naeem847 <ashoornaeem@gmail.com>
2026-07-24 23:59:45 +0500

    A beginner-friendly Linear Regression project using Python, Pandas, Matplotlib, and Scikit-learn to predict employee salaries based on age.

--- linear_regration.py
+++ linear_regration.py
@@ -1,5 +1,50 @@
 import pandas as pd
+
 import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
 
 from sklearn.linear_model import LinearRegression
+
+df=pd.read_csv('D:\object_oriented_programming1.py\hope-to-skills.py\employees_of_software.csv')
+print(df)
+
+# select the numeric cols for regression
+# numeric_cols=df.select_dtypes(include=['int64','float64']).columns.tolist()
+
+# # numeric_cols.remove('Id')
+# print(numeric_cols)
+# define the features X and terget Y
+X=df[['age']]
+Y=df['salary']
+
+# split data into training and testing
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.3,random_state=0.42)
+
+# create and train the linear regretion model
+model=LinearRegression()
+model.fit(X_train,Y_train)
+
+# predict on test date
+
+Y_pred=model.predict(X_test)
+
+# plot regression line
+plt.scatter(X_test,Y_test,color='blue',label='actual Date')
+
+plt.plot(X_test,Y_pred,color='red',linewidth=2,label='regression')
+
+plt.xlabel("Age")
+
+plt.xlabel("Salary")
+
+plt.title("linear regression: Age Vs salary")
+plt.legend()
+plt.show()
+
+# Display model coefficients
+print("\nmodel coefficient:")
+
+print(f"model intercept:{model.intercept_}")
+
+print(f"slope:{model.coef_[0]}")
