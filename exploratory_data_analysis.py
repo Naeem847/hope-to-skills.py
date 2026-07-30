dee4b4a1b80f1e753605793b30a054abf8774d42
Naeem847 <ashoornaeem@gmail.com>
2026-07-30 23:35:21 +0500

    working EDA

---
+++ exploratory_data_analysis.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import sklearn
+
+import seaborn as sns
+
+import warnings
+
+warnings.filterwarnings('ignore')
+
+plt.rcParams["figure.figsize"]=[10,5]
+
+# ignore warnigs
+
+import warnings
+
+# set the warning filters to ignore futurewarnigs
+warnings.simplefilter(action="ignore",category=FutureWarning)
