3f1761d23fc72a797bb9b9a3776f22172a0d1a55
Naeem847 <ashoornaeem@gmail.com>
2026-07-31 23:54:57 +0500

    This repository contains my Exploratory Data Analysis (EDA) practice using the Titanic dataset in Python. It covers data loading with Pandas, data visualization using Matplotlib, Seaborn, and Plotly, including count plots, histograms, color palettes, and interactive charts. This project helped me practice analyzing passenger survival patterns based on class, gender, and age while improving my data visualization and EDA skills.

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -65,10 +65,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 # create figure and axis
 plt.figure(figsize=(10,6))
-sns.countplot(x='sex', hue='Survived', data=df)
+sns.countplot(x='sex', hue='survived', data=df)
 plt.title('survival rate by Gender')
 plt.xlabel('Gender')
 plt.ylabel('count')
 plt.legend(labels=['did not survive','Survived'])
 print(plt.show())
 print(df.columns)
+
+#age distribution of passengers
+print("age distribution of passengers")
+plt.figure(figsize=(12,6))
+sns.histplot(data=df,x='age',bins=30,kde=True)
+plt.title('Age distribution of passengers')
+plt.xlabel('age')
+plt.ylabel('count')
+plt.axvline(df['age'].mean(),color='red',linestyle='--',label='Mean age')
+plt.legend()
+print(plt.show())
+# maing dynamic graph using plotly and plotly express
+import pandas as pd
+import plotly.express as px
+# 1.Bar chart count of survived  
+fig1=px.bar(df,x="survived",title="survived  count",labels={"survived":"survived(0=NO,1=yes)"})
+fig1.show()
+# 2.histogram age distribution
+fig2=px.histogram(df,x="age",nbins=30,title="age distribution of passengers",labels={"age":"age"})
+print(fig2.show())
