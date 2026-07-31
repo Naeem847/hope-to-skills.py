9d82b29ed3b88b9dd87452cf04c31765015d21ae
Naeem847 <ashoornaeem@gmail.com>
2026-07-31 18:10:37 +0500

    Exploratory Data Analysis (EDA) using Python on the Titanic dataset. This repository demonstrates data loading, visualization with Matplotlib and Seaborn, color palettes, and count plots to analyze passenger survival by class and gender.

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -20,3 +20,55 @@ import warnings
 
 # set the warning filters to ignore futurewarnigs
 warnings.simplefilter(action="ignore",category=FutureWarning)
+
+
+# file_path=('eda_data.csv')
+
+df=pd.read_csv('eda_data.csv')
+# display basic information
+
+print(df.info())
+print(df.head())
+print("total data")
+print(df)
+print("show the seaborn functionality")
+current_palette1=sns.color_palette()
+sns.palplot(current_palette1)
+print(plt.show())
+
+print("continous coolor palettes")
+# current_palette2=sns.color_palette()
+
+sns.palplot(sns.color_palette('colorblind'))
+print(plt.show())
+
+# print the colorblind people 
+current_palette2=sns.color_palette()
+
+sns.palplot(sns.color_palette('colorblind'))
+print(plt.show())
+
+# simple graph
+import matplotlib.pyplot as plt
+import seaborn as sns
+# create figure and axis
+plt.figure(figsize=(10,6))
+sns.countplot(x='pclass', hue='survived', data=df)
+plt.title('survival rate by passenger class')
+plt.xlabel('passenger class')
+plt.ylabel('count')
+plt.legend(labels=['did not survive','survived'])
+print(plt.show())
+# Survival rate by gender
+df=pd.read_csv('eda_data.csv')
+import matplotlib.pyplot as plt
+import seaborn as sns
+# create figure and axis
+plt.figure(figsize=(10,6))
+sns.countplot(x='sex', hue='Survived', data=df)
+plt.title('survival rate by Gender')
+plt.xlabel('Gender')
+plt.ylabel('count')
+plt.legend(labels=['did not survive','Survived'])
+print(plt.show())
+print(df.columns)
