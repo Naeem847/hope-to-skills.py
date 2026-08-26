fbf9dc7b09d09b725391c94d8b9cf153fc234fbe
Naeem847 <ashoornaeem@gmail.com>
2026-08-26 19:52:09 +0500

    This repository contains a Python script for predicting diabetes outcomes using machine learning with the standard Pima Indians Diabetes Dataset.  Key Features Data Preprocessing: Handles missing values dynamically using median imputation for numerical features and mode imputation for categorical features.  Feature Scaling: Prepares datasets for distance-based models using StandardScaler.  Classification Model: Trains a Support Vector Machine (SVC) with a linear kernel to classify diabetes outcomes.  Model Evaluation: Performance is measured using Accuracy, Classification Report (Precision, Recall, F1-Score), and a Confusion Matrix.

--- knn.py
+++ knn.py
@@ -11,7 +11,23 @@ df['target']=iris.target
 # display the dataframe
 print(df.head())
 from sklearn.model_selection import train_test_split
-x=df.drop('terget',axis=1)
+x=df.drop('target',axis=1)
 y=df['target']
 # assuming x and y are defined
-X_train,x_test,y_train,y_test=train_test_split(x,y,test)
+X_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+# create the model
+model=neighbors.KNeighborsClassifier(n_neighbors=3)
+model.fit(X_train,y_train)
+from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+# predict on test data
+y_pred=model.predict(x_test)
+# calculate accuracy
+accuracy=accuracy_score(y_test,y_pred)
+print(f"model accuracy:{accuracy:.2f}")
+
+# classification report
+print("\nclassification_report")
+print(classification_report(y_test,y_pred))
+
+# confision matrics
+print(confusion_matrix(y_test,y_pred))
---
+++ svm.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+# load the dataset(update the file path to your local path)
+file_path="diabetes.csv"
+df=pd.read_csv(file_path)
+# display basic information
+print("dataset information")
+print(df.info())
+print("\nfirst 5 rows")
+print(df.head())
+# checking the missing value
+print(df.isnull().sum())
+# fill the missing numerical ValueError
+df.fillna(df.median(numeric_only=True),inplace=True)
+# filling ,missing categorical values with the mode(if any)
+for col in df.select_dtypes(include=['object']):
+    df[col].fillna(df[col].mode(),[0],inplace=True)
+# step 3: prepare data
+# separate the fetureb and target variable
+X=df.drop(columns=['Outcome'])
+y=df['Outcome']
+# apply standard scalling
+scaler= StandardScaler()
+X_scaled=scaler.fit_transform(X)
+# step 5:split data into training and testing set
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=42)
+# step 6:modified train a support vector machine classifier
+from sklearn.svm import SVC
+print("\ntraining svm classifier")
+svm_model=SVC(kernel='linear',random_state=42)
+svm_model.fit(X_train,y_train)
+# model evaluation for SVM
+y_pred_svm=svm_model.predict(X_test)
+# calculate acuracy
+svm_accuracy=accuracy_score(y_test,y_pred_svm)
+
+print(f"\nSVM model sccuracy:{svm_accuracy:.2f}")
+# classification report
+print("\nclassification report")
+print(classification_report(y_test,y_pred_svm))
+# confusion matrix
+print("\nconfusion matrix")
+print(confusion_matrix(y_test,y_pred_svm))
